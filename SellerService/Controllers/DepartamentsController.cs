514a5b011df58d2a52c600bafeab27ff1bdc5aba
bgalvao90 <faculdadebgalvas@gmail.com>
2025-04-08 22:33:37 -0300

    Adicionar arquivos de projeto.

---
+++ SellerService/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Electronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+
+            return View(list);
+        }
+    }
+}
