62eebcc8a4521415668d5f730992b4d8fd8ba39f
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-04-10 14:35:08 -0300

    Delete seller

--- SellerService/Services/SellersService.cs
+++ SellerService/Services/SellersService.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-
+        
         public List<Sellers> FindAll()
         {
             return _context.Seller.ToList();
@@ -24,6 +24,18 @@ namespace SalesWebMvc.Services
         {
             obj.Departament = _context.Departament.First();
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Sellers FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
