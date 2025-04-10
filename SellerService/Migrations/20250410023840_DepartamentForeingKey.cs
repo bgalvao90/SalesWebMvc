4e2126bed7a3a45028802f14d0c1fc4372d88a2a
bgalvao90 <faculdadebgalvas@gmail.com>
2025-04-09 23:41:15 -0300

    Foreign key not null

---
+++ SellerService/Migrations/20250410023840_DepartamentForeingKey.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartamentForeingKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentId",
+                table: "Seller",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller",
+                column: "DepartamentId",
+                principalTable: "Departament",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentId",
+                table: "Seller",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller",
+                column: "DepartamentId",
+                principalTable: "Departament",
+                principalColumn: "Id");
+        }
+    }
+}
